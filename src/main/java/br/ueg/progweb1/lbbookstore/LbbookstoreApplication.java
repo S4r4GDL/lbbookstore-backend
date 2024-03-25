da2898e17dbb62a3413120a03ba9693e2ef9dab5
S4r4GDL <saragdl7321@gmail.com>
2024-03-25 17:22:19 -0300

    FIX - LbbookstoreApplication.java.java

--- src/main/java/br/ueg/progweb1/lbbookstore/LbbookstoreApplication.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/LbbookstoreApplication.java
@@ -2,10 +2,10 @@ package br.ueg.progweb1.lbbookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class LbbookstoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LbbookstoreApplication.class, args);
 	}
