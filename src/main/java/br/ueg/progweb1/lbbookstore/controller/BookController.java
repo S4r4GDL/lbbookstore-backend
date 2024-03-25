a7259f879cf0ddecdca04d0f3da35b166e8da64a
S4r4GDL <saragdl7321@gmail.com>
2024-03-25 16:05:59 -0300

    ADD - BookController.java

---
+++ src/main/java/br/ueg/progweb1/lbbookstore/controller/BookController.java
@@ -0,0 +1,11 @@
+package br.ueg.progweb1.lbbookstore.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "${api.version}/student")
+public class BookController {
+
+}
