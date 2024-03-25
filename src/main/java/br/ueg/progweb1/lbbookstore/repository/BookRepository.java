820c629c1ecdc4528f7d98e56f9ad7873d3e5d06
S4r4GDL <saragdl7321@gmail.com>
2024-03-25 17:22:02 -0300

    FIX - BookRepository.java

--- src/main/java/br/ueg/progweb1/lbbookstore/repository/BookRepository.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/repository/BookRepository.java
@@ -1,10 +1,9 @@
 package br.ueg.progweb1.lbbookstore.repository;
 
+import br.ueg.progweb1.lbbookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-    public interface BookRepository<Book> extends JpaRepository<Book, Long> {
-
-
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
