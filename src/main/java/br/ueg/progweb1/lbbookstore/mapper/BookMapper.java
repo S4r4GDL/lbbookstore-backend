3620b499c20fc3a480f016ba7a0d9bf55d79e1b3
S4r4GDL <saragdl7321@gmail.com>
2024-03-25 16:44:49 -0300

    UPDATE - Aplication properties + *BookMapper

--- src/main/java/br/ueg/progweb1/lbbookstore/mapper/BookMapper.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/mapper/BookMapper.java
@@ -1,22 +1,37 @@
 package br.ueg.progweb1.lbbookstore.mapper;
 
 import br.ueg.progweb1.lbbookstore.model.Book;
+import br.ueg.progweb1.lbbookstore.model.dto.BookReadDTO;
 import br.ueg.progweb1.lbbookstore.model.dto.CreateBookDTO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookMapper {
-    public Book toModel(CreateBookDTO bookDTO){
+    public Book toModel(CreateBookDTO bookCreateDTO){
         Book book = new Book();
-        book.setAuthor(bookDTO.getAuthor());
-        book.setTitle(bookDTO.getTitle());
-        book.setPublisher(bookDTO.getPublisher());
-        book.setEdition(bookDTO.getEdition());
-        book.setReleaseYear(bookDTO.getReleaseYear());
-        book.setPrice(bookDTO.getPrice());
-        book.setQuantity(bookDTO.getQuantity());
-        book.setActive(bookDTO.getActive());
-        book.setLastUpdate(bookDTO.getLastUpdate());
+        book.setAuthor(bookCreateDTO.getAuthor());
+        book.setTitle(bookCreateDTO.getTitle());
+        book.setPublisher(bookCreateDTO.getPublisher());
+        book.setEdition(bookCreateDTO.getEdition());
+        book.setReleaseYear(bookCreateDTO.getReleaseYear());
+        book.setPrice(bookCreateDTO.getPrice());
+        book.setQuantity(bookCreateDTO.getQuantity());
+        book.setActive(bookCreateDTO.getActive());
+        book.setLastUpdate(bookCreateDTO.getLastUpdate());
         return book;
     }
+    public BookReadDTO toDTO(Book book){
+        BookReadDTO bookReadDTO = new BookReadDTO();
+        bookReadDTO.setAuthor(book.getAuthor());
+        bookReadDTO.setTitle(book.getTitle());
+        bookReadDTO.setPublisher(book.getPublisher());
+        bookReadDTO.setEdition(book.getEdition());
+        bookReadDTO.setReleaseYear(book.getReleaseYear());
+        bookReadDTO.setPrice(book.getPrice());
+        bookReadDTO.setQuantity(book.getQuantity());
+        bookReadDTO.setActive(book.getActive());
+        bookReadDTO.setLastUpdate(book.getLastUpdate());
+        return bookReadDTO;
+    }
+
 }
