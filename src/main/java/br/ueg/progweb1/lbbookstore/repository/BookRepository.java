7369696484e876afca50e4f7820857b375cd5bd6
S4r4GDL <saragdl7321@gmail.com>
2024-03-26 16:06:00 -0300

    ADD - Methods: findAllByActive

--- src/main/java/br/ueg/progweb1/lbbookstore/repository/BookRepository.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/repository/BookRepository.java
@@ -4,6 +4,9 @@ import br.ueg.progweb1.lbbookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findAllByActive(Boolean active);
 }
