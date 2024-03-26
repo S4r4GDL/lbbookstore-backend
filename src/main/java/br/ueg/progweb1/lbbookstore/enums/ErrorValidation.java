f83225a02878eda4cf6fae2a25e26007d740c7a8
S4r4GDL <saragdl7321@gmail.com>
2024-03-26 17:02:13 -0300

    ADD - Finals: INVALID_QUANTITY_TO_DELETE

--- src/main/java/br/ueg/progweb1/lbbookstore/enums/ErrorValidation.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/enums/ErrorValidation.java
@@ -10,7 +10,8 @@ public enum ErrorValidation {
     NOT_FOUND(404L, "Not found!"),
     MANDATORY_FIELD_VIOLATION(3L, "Mandatory field not filled!"),
     INVALID_PRICE(4L, "The price must be higher than 0"),
-    INVALID_QUANTITY(5L, "The quantity must be higher or equals to 0");
+    INVALID_QUANTITY(5L, "The quantity must be higher or equals to 0"),
+    INVALID_QUANTITY_TO_DELETE(6L, "The quantity must be 0 so you can delete");
 
     private Long code;
     private String message;
