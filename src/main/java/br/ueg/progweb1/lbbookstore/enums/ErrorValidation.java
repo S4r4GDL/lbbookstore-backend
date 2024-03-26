7bf25d61a1d73a482cfaded609d83e51741154ee
S4r4GDL <saragdl7321@gmail.com>
2024-03-26 16:03:05 -0300

    FIX - ErrorValidations.java

---
+++ src/main/java/br/ueg/progweb1/lbbookstore/enums/ErrorValidation.java
@@ -0,0 +1,22 @@
+package br.ueg.progweb1.lbbookstore.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorValidation {
+
+    GENERAL(0L, "Undefined error!"),
+    INVALID_ID(400L, "Invalid Id!"),
+    NOT_FOUND(404L, "Not found!"),
+    MANDATORY_FIELD_VIOLATION(3L, "Mandatory field not filled!"),
+    INVALID_PRICE(4L, "The price must be higher than 0"),
+    INVALID_QUANTITY(5L, "The quantity must be higher or equals to 0");
+
+    private Long code;
+    private String message;
+
+    ErrorValidation(Long code, String message){
+        this.code = code;
+        this.message = message;
+    }
+}
