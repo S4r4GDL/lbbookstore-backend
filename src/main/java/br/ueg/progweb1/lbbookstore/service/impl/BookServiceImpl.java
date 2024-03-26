8a047df57052aee47e2ff6682132f999d37376ec
S4r4GDL <saragdl7321@gmail.com>
2024-03-26 14:08:31 -0300

    ADD - Methods : prepareToCreate validateBusinessLogicToCreate validateMandatoryFields + FIX - Method: create

--- src/main/java/br/ueg/progweb1/lbbookstore/service/impl/BookServiceImpl.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/service/impl/BookServiceImpl.java
@@ -1,17 +1,43 @@
 package br.ueg.progweb1.lbbookstore.service.impl;
 
 import br.ueg.progweb1.lbbookstore.model.Book;
+import br.ueg.progweb1.lbbookstore.repository.BookRepository;
 import br.ueg.progweb1.lbbookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
+
+    @Autowired
+    BookRepository repository;
     @Override
     public Book create(Book newBook) {
-        return null;
+        validateMandatoryFields(newBook);
+        validateBusinessLogicToCreate(newBook);
+        prepareToCreate(newBook);
+        return repository.save(newBook);
+    }
+
+    private void prepareToCreate(Book newBook) {
+        newBook.setId(0L);
+        newBook.setLastUpdate(LocalDate.now());
+    }
+
+    private void prepareToUpdate(Book newBook) {
+        newBook.setLastUpdate(LocalDate.now());
+    }
+
+    private void validateBusinessLogicToCreate(Book newBook) {
+
+
+    }
+
+    private void validateMandatoryFields(Book newBook) {
     }
 
     @Override
