da9c905bb3c7cfca1bb84379a292c32a94aacea7
S4r4GDL <saragdl7321@gmail.com>
2024-03-26 16:08:13 -0300

    FIX - Methods: validateBusinessLogicToCreate and validateMandatoryFields ADD - Methods: validateId, delete and getActiveBooks

--- src/main/java/br/ueg/progweb1/lbbookstore/service/impl/BookServiceImpl.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/service/impl/BookServiceImpl.java
@@ -1,5 +1,7 @@
 package br.ueg.progweb1.lbbookstore.service.impl;
 
+import br.ueg.progweb1.lbbookstore.enums.ErrorValidation;
+import br.ueg.progweb1.lbbookstore.exception.BusinessLogicException;
 import br.ueg.progweb1.lbbookstore.model.Book;
 import br.ueg.progweb1.lbbookstore.repository.BookRepository;
 import br.ueg.progweb1.lbbookstore.service.BookService;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -34,10 +38,19 @@ public class BookServiceImpl implements BookService {
 
     private void validateBusinessLogicToCreate(Book newBook) {
 
+        if(newBook.getPrice().intValue() <= 0)
+            throw new BusinessLogicException(ErrorValidation.INVALID_PRICE);
+        if(newBook.getQuantity() < 0)
+            throw new BusinessLogicException(ErrorValidation.INVALID_QUANTITY);
 
     }
 
-    private void validateMandatoryFields(Book newBook) {
+    private void validateMandatoryFields(Book bookData) {
+        if(bookData.getAuthor().isBlank()
+                || bookData.getPublisher().isBlank()
+                || bookData.getEdition().isBlank()
+                || bookData.getTitle().isBlank())
+            throw new BusinessLogicException(ErrorValidation.MANDATORY_FIELD_VIOLATION);
     }
 
     @Override
@@ -47,12 +60,22 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public List<Book> listAll() {
-        return null;
+        return repository.findAll();
     }
 
     @Override
     public Book getById(Long id) {
-        return null;
+        return validateId(id);
+    }
+
+    private Book validateId(Long id) {
+
+        Optional<Book> book = repository.findById(id);
+
+        if(book.isEmpty())
+            throw new BusinessLogicException(ErrorValidation.INVALID_ID);
+
+        return book.get();
     }
 
     @Override
@@ -68,5 +91,15 @@ public class BookServiceImpl implements BookService {
     @Override
     public List<Book> listLowerPrice(BigDecimal limitPrice) {
         return null;
+    }
+
+    @Override
+    public Boolean delete(Book model) {
+        return true;
+    }
+
+    @Override
+    public List<Book> getActiveBooks() {
+        return repository.findAllByActive(true);
     }
 }
