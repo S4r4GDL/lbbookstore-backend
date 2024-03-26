48c8ad774b69a8750489dd79bb4b2a08fa758cb8
S4r4GDL <saragdl7321@gmail.com>
2024-03-26 16:06:46 -0300

    ADD - Methods: delete and getActiveBooks

--- src/main/java/br/ueg/progweb1/lbbookstore/service/BookService.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/service/BookService.java
@@ -4,15 +4,24 @@ import br.ueg.progweb1.lbbookstore.model.Book;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookService {
     Book create(Book newBook);
+
     Book update(Book book);
+
     List<Book> listAll();
-    Book getById( Long id);
+
+    Book getById(Long id);
+
     List<Book> getByAuthor(String author);
 
     List<Book> getByPublisher();
 
     List<Book> listLowerPrice(BigDecimal limitPrice);
+
+    Boolean delete(Book model);
+
+    List<Book> getActiveBooks();
 }
