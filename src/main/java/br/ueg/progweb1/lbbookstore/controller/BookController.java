fb257a16e1becc858c766d7df0dc8864c3a4b846
S4r4GDL <saragdl7321@gmail.com>
2024-03-26 14:03:13 -0300

    ADD - Method post

--- src/main/java/br/ueg/progweb1/lbbookstore/controller/BookController.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/controller/BookController.java
@@ -1,28 +1,43 @@
 package br.ueg.progweb1.lbbookstore.controller;
 
 
+import br.ueg.progweb1.lbbookstore.mapper.BookMapper;
+import br.ueg.progweb1.lbbookstore.model.dto.BookCreateDTO;
+import br.ueg.progweb1.lbbookstore.service.BookService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "${api.version}/book")
 public class BookController {
+    @Autowired
+    BookService service;
+
+    @Autowired
+    BookMapper mapper;
+
     @GetMapping
     @Operation(description = "End point to list all the books")
-    public ResponseEntity getAllBooks()
+    public ResponseEntity<Object> getAllBooks()
     {
         var response = "Get working";
         return ResponseEntity.ok(response);
     }
     @GetMapping(path = "/{id}")
     @Operation(description = "End point to get a book by id")
-    public ResponseEntity getBookById(@PathVariable String id)
+    public ResponseEntity<Object> getBookById(@PathVariable String id)
     {
         var response = "Get book by id working" + id;
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    @Operation(description = "End point to create a new book")
+    public ResponseEntity<Object> createBook(@RequestBody BookCreateDTO bookDTO)
+    {
+        var response = service.create(mapper.toModel(bookDTO));
         return ResponseEntity.ok(response);
     }
 
