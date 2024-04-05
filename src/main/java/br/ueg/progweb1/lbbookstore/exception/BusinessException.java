a7ade2f236966bd275fa68c3f27455fb25591c0c
sarag <saragdl@gmail.com>
2024-04-05 17:54:40 -0300

    FIX - Not used methods

--- src/main/java/br/ueg/progweb1/lbbookstore/exception/BusinessException.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/exception/BusinessException.java
@@ -5,14 +5,7 @@ import lombok.Getter;
 
 public @Getter class BusinessException extends RuntimeException{
     private ErrorValidation error;
-    public BusinessException(String message, Throwable e){
-        super(message, e);
-        this.error = ErrorValidation.GENERAL;
-    }
-    public BusinessException(String message){
-        super(message);
-        this.error = ErrorValidation.GENERAL;
-    }
+
     public BusinessException(ErrorValidation error){
         super(error.getMessage());
         this.error = error;
