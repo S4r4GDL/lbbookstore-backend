8bb7d8d05ec18e16e08f39bea5b9503aa43d7b42
sarag <saragdl@gmail.com>
2024-04-29 10:17:04 -0300

    ADD - Product.java

---
+++ src/main/java/br/ueg/progweb1/lbbookstore/model/Product.java
@@ -0,0 +1,36 @@
+package br.ueg.progweb1.lbbookstore.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@SuperBuilder
+@EqualsAndHashCode(of="id")
+@MappedSuperclass
+public class Product implements GenericModel<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="id")
+    protected Long id;
+
+    @Column(name="price", nullable = false)
+    protected BigDecimal price;
+
+    @Column(name="quantity", nullable = false)
+    protected Integer quantity;
+
+    @Column(name="last_Updade", nullable = false)
+    protected LocalDate lastUpdate;
+
+    @Column(name="active", nullable = false)
+    protected Boolean active;
+
+
+}
