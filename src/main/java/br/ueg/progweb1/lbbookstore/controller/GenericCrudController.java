f473ecc0dc55ed7d2cccd1dc6a7b88af948714d6
sarag <saragdl@gmail.com>
2024-05-16 13:50:24 -0300

    ADD - delete more items ADD - mapper fromDTOToModel

--- src/main/java/br/ueg/progweb1/lbbookstore/controller/GenericCrudController.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/controller/GenericCrudController.java
@@ -1,8 +1,6 @@
 package br.ueg.progweb1.lbbookstore.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
