a2ef166c44ce77205ddc27d2a315b0b7f5f75595
sarag <saragdl@gmail.com>
2024-06-25 15:47:47 -0300

    ADD - adding authentication using JWT and oauth2

--- src/main/java/br/ueg/progweb1/lbbookstore/repository/UserRepository.java
+++ src/main/java/br/ueg/progweb1/lbbookstore/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package br.ueg.progweb1.lbbookstore.repository;
 
-import br.ueg.progweb1.lbbookstore.model.cart.Cart;
 import br.ueg.progweb1.lbbookstore.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
